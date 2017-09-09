673e9900ba9a2c9dd200cf14ab5e13758eb7c58f
María Andrea Villada Gaviria <patashouse@gmail.com>
2017-09-08 22:03:17 -0300

    Primera parte de ejercicios de js en el browser

---
+++ js-browser/ej15/script.js
@@ -0,0 +1,6 @@
+const element= document.getElementById('importante');
+console.log(element);
+const elements=document.querySelectorAll('li > a');
+console.log(elements);
+const otroElemento=document.querySelector('ul');
+console.log(otroElemento);
